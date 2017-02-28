f807449e47f8bbea0042e3d38a71b3ad2f365b56
Christian Hernandez <christian.hernandez@redhat.com>
2017-02-28 15:07:55 -0800

    testing color switch

--- image.php
+++ image.php
@@ -2,12 +2,19 @@
 // Create a 55x30 image
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
-$blue = imagecolorallocate($im, 0, 0, 255);
-$green = imagecolorallocate($im, 0, 255, 0);
+
+$deployment = getenv("COLOR");
+
+if ( $deployment == 'blue') {
+  $color = imagecolorallocate($im, 0, 0, 255);
+} elseif ($deployment == 'green')  {
+  $color = imagecolorallocate($im, 0, 255, 0);
+} else {
+  $color = imagecolorallocate($im, 0, 0, 255);
+}
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-//imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
 header('Content-Type: image/png');
