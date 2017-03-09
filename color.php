52f1db3a9e27be48153f39a9225fd9ed8deb5975
Christian Hernandez <christianh814@users.noreply.github.com>
2017-03-09 13:22:04 -0800

    Default color to blue if not specified

--- color.php
+++ color.php
@@ -1,5 +1,15 @@
 <?php
 $deployment = getenv("COLOR");
+
+$deployment = getenv("COLOR");
+if ( $deployment == 'blue') {
+  $color = 'blue';
+} elseif ($deployment == 'green')  {
+  $color = 'green';
+} else {
+  $color = 'blue';
+}
+
 $data = [ 'color' => $deployment ];
 echo json_encode( $data );
 http_response_code(200);
