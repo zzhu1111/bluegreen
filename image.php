9008f891345dc7d4bbbe8be0c88926d5fb684ee4
Samuel Tauil <samueltauil@gmail.com>
2019-02-20 00:53:06 -0500

    adding a comment line
    to be used by webhook lab

--- image.php
+++ image.php
@@ -2,7 +2,7 @@
 // Create a 55x30 image
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
-
+// Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
